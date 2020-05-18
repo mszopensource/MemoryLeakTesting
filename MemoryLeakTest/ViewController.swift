e88117056497f4e53d3f2d4a312e46f8c3fff7e1
Shunzhe Ma <me@msz.cool>
2020-05-18 12:33:37 -0400

    Initial Commit

---
+++ MemoryLeakTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryLeakTest
+//
+//  Created by Shunzhe Ma on 5/18/20.
+//  Copyright © 2020 Shunzhe Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
